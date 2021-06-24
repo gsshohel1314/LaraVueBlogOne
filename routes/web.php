95cfb8e7282bd4354fb1c1e7cc42c927a356fcdd
gs shohel1314 <gsshohel1314@gmail.com>
2021-06-24 18:40:10 +0600

    setup layouts

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::view('dashboard', 'backend.dashboard');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
